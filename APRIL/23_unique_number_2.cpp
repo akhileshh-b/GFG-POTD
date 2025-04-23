51bbd825fb2089f4f388caaae81401ec64688139
akhileshh <akhileshbonde02@gmail.com>
2025-04-23 12:00:00 +0530

    Added Unique Number II solution

---
+++ APRIL/23_unique_number_2.cpp
@@ -0,0 +1,13 @@
+// 23. Unique Number II
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> singleNum(vector<int>& A) {
+        int x = 0, a = 0, b = 0;
+        for (int n : A) x ^= n;
+        for (int n : A) (n & (x & -x) ? a : b) ^= n;
+        return a < b ? vector<int>{a, b} : vector<int>{b, a};
+    }
+};
