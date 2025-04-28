90557c5178685d15f0fa6355fbd3d1d729f94ff3
akhileshh <akhileshbonde02@gmail.com>
2025-04-28 12:00:00 +0530

    Added Maximum Sum of Non-Adjacent Nodes solution

---
+++ APRIL/28_max_sum_non_adjacent_nodes.cpp
@@ -0,0 +1,22 @@
+// 28. Maximum Sum of Non-Adjacent Nodes
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class Solution {
+public:
+    pair<int,int> solve(Node* root) {
+        if (!root) return {0,0};
+        auto l = solve(root->left), r = solve(root->right);
+        return {root->data + l.second + r.second, max(l.first,l.second) + max(r.first,r.second)};
+    }
+    int getMaxSum(Node* root) {
+        auto p = solve(root);
+        return max(p.first, p.second);
+    }
+};
