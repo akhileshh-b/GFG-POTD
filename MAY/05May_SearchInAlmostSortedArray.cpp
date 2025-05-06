81ba0ec85a4ea3e974567dc6af7e7ec0c5cc0657
Akhilesh Bonde <110801340+akhileshh-b@users.noreply.github.com>
2025-05-06 21:31:45 +0530

    Delete MAY directory

--- MAY/05May_SearchInAlmostSortedArray.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-  public:
-    int findTarget(vector<int>& arr, int target) {
-        for(int i = 0; i < arr.size(); ++i)
-            if(arr[i] == target) return i;
-        return -1;
-    }
-}; 
